cb47ff669c4cfd392405111e59e8635cc59c68b2
XtremerX2801 <vdho12345678@gmail.com>
2023-06-07 22:24:04 +0700

    New button

---
+++ quiz-ui/src/components/Button.js
@@ -0,0 +1,10 @@
+export default function Button()
+{
+    return (
+        <>
+            <button type="button" class="text-white bg-blue-700 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                Default
+            </button>
+        </>
+    )
+}
