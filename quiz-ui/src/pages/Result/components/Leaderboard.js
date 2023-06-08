6ae22c09a2c17e9650fd1008efcefd16c80f89a6
XtremerX2801 <vdho12345678@gmail.com>
2023-06-08 21:24:30 +0700

    Add leaderboard

---
+++ quiz-ui/src/pages/Result/components/Leaderboard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Leaderboard = ({ data }) => {
+  if (!data || data.length === 0) {
+    return <p>No data available</p>;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Position</th>
+          <th>Name</th>
+          <th>School</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((player, index) => (
+          <tr key={player.id}>
+            <td>{index + 1}</td>
+            <td>{player.name}</td>
+            <td>{player.school}</td>
+            <td>{player.score}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+Leaderboard.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      school: PropTypes.string.isRequired,
+      score: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default Leaderboard;
