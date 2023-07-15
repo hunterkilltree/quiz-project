7325449138a6da0aa35acab5e56c9972e8a3d78e
XtremerX2801 <vdho12345678@gmail.com>
2023-07-15 13:11:57 +0700

    Change navbar layout and hover effect

--- quiz-ui/src/components/NavBar/NavBar.js
+++ quiz-ui/src/components/NavBar/NavBar.js
@@ -19,77 +19,111 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className="navigation">
-        <img
-          src="https://images.squarespace-cdn.com/content/v1/5e7edd655234e75594647807/1586498728796-Q9USRXPO6FFZOCYXAD57/Screen+Shot+2020-04-10+at+3.35.11+pm.png?format=1500w"
-          alt="Logo"
-          onClick={handleImageClick}
-        />
-
-        <button
-          className="hamburger"
-          onClick={() => {
-            setIsNavExpanded(!isNavExpanded);
-          }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="white">
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
-        <div className={isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'}>
-          <ul>
-            <li>
-              <Link
-                onClick={() => {
-                  setIsNavExpanded(false);
-                }}
-                to="/Home"
-                className={location.pathname === '/Home' ? 'nav-active' : 'nav-non-active'}>
-                <div>
-                  {' '}
-                  <FlutterDashIcon />
-                </div>
-                <div className="nav-text-icon">Home</div>
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => {
-                  setIsNavExpanded(false);
-                }}
-                to="/Quiz"
-                className={location.pathname === '/Quiz' ? 'nav-active' : 'nav-non-active'}>
-                <div>
-                  {' '}
-                  <ScienceIcon />
-                </div>
-                <div className="nav-text-icon">Quiz</div>
-              </Link>
-            </li>
-            <li>
-              <Link
-                onClick={() => {
-                  setIsNavExpanded(false);
-                }}
-                to="/Leaderboard"
-                className={location.pathname === '/Leaderboard' ? 'nav-active' : 'nav-non-active'}>
-                <div>
-                  {' '}
-                  <DashboardIcon />
-                </div>
-                <div className="nav-text-icon">Leaderboard</div>
-              </Link>
-            </li>
-          </ul>
+      <div>
+        <div style={{display: 'flex'}}>
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5e7edd655234e75594647807/1586498728796-Q9USRXPO6FFZOCYXAD57/Screen+Shot+2020-04-10+at+3.35.11+pm.png?format=1500w"
+            alt="Logo"
+            onClick={handleImageClick}
+            style={{margin: 'auto', width: '10%'}}
+          />
         </div>
-      </nav>
+        <div style={{display: 'flex'}}>
+          <nav className="navigation">
+            <button
+              className="hamburger"
+              onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+              }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="white">
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            <div className={isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'}>
+              <ul>
+                <li>
+                  <Link
+                    onClick={() => {
+                      setIsNavExpanded(false);
+                    }}
+                    to="/Home"
+                    className={location.pathname === '/Home' ? 'nav-active' : 'nav-non-active'}>
+                    <div>
+                      {' '}
+                      <FlutterDashIcon />
+                    </div>
+                    <div className="nav-text-icon hover-underline-animation">Home</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => {
+                      setIsNavExpanded(false);
+                    }}
+                    to="/Quiz"
+                    className={location.pathname === '/Quiz' ? 'nav-active' : 'nav-non-active'}>
+                    <div>
+                      {' '}
+                      <ScienceIcon />
+                    </div>
+                    <div className="nav-text-icon hover-underline-animation">Quiz</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => {
+                      setIsNavExpanded(false);
+                    }}
+                    to="/Leaderboard"
+                    className={location.pathname === '/Leaderboard' ? 'nav-active' : 'nav-non-active'}>
+                    <div>
+                      {' '}
+                      <DashboardIcon />
+                    </div>
+                    <div className="nav-text-icon hover-underline-animation">Leaderboard</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => {
+                      setIsNavExpanded(false);
+                    }}
+                    to="/Leaderboard"
+                    className={location.pathname === '/Leaderboard' ? 'nav-active' : 'nav-non-active'}>
+                    <div>
+                      {' '}
+                      <DashboardIcon />
+                    </div>
+                    <div className="nav-text-icon hover-underline-animation">National Science Week</div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => {
+                      setIsNavExpanded(false);
+                    }}
+                    to="/Leaderboard"
+                    className={location.pathname === '/Leaderboard' ? 'nav-active' : 'nav-non-active'}>
+                    <div>
+                      {' '}
+                      <DashboardIcon />
+                    </div>
+                    <div className="nav-text-icon hover-underline-animation">Contact Us</div>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
