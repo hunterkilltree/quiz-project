7bb4c6e214748448d2811a1727f95d0580977fda
hunterkilltree <snguoila@yahoo.com>
2023-07-23 11:12:15 +0700

    Add mui core style

--- quiz-ui/src/App.js
+++ quiz-ui/src/App.js
@@ -6,6 +6,13 @@ import { NavBar } from './components/NavBar';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { Leaderboard } from './pages/Leaderboard';
 import Footer from './components/Footer';
+import { styled } from '@material-ui/core';
+
+const MainStyle = styled('main')(({ theme }) => ({
+  flexGrow: 1,
+  minHeight: '100vh',
+  padding: theme.spacing(0)
+}));
 
 export const App = () => {
   const navigate = useNavigate();
@@ -19,7 +26,7 @@ export const App = () => {
   }, [location.pathname, navigate]);
 
   return (
-    <div>
+    <MainStyle>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -29,6 +36,6 @@ export const App = () => {
         <Route path="/Result" element={<Result />} />
       </Routes>
       <Footer />
-    </div>
+    </MainStyle>
   );
 };
